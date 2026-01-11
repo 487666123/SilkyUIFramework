12ca9325a54c071f18909af28228144e71651cd6
太阳照常升起 <487666123@qq.com>
2026-01-11 16:05:44 +0800

    v2601111

--- CursorSnippet.cs
+++ CursorSnippet.cs
@@ -2,21 +2,23 @@ using Terraria.UI.Chat;
 
 namespace SilkyUIFramework;
 
-public class CursorSnippet : TextSnippet
+/// <summary>
+/// 输入框光标
+/// </summary>
+public class CursorSnippet(SUIEditText editText) : TextSnippet(" ")
 {
-    private readonly SUIEditText _editText;
+    private readonly SUIEditText _editText = editText;
     public DynamicSpriteFont Font { get; set; }
 
+    /// <summary>
+    /// 特殊适配高度，我也不想这么写，但是这个底层架构我还能怎么办，啊啊啊啊啊啊啊啊啊
+    /// </summary>
     public float TrueHeight { get; set; }
 
-    public CursorSnippet(SUIEditText editText)
-    {
-        Text = " ";
-        _editText = editText;
-    }
-
+    // 宽度计算
     public override float GetStringLength(DynamicSpriteFont font) => 0f;
 
+    // 原版很逆天的使用 Draw 方法来计算大小, 用一个 bool 参数来区分是在计算还是在绘制
     public override bool UniqueDraw(bool justCheckingString,
         out Vector2 size, SpriteBatch spriteBatch, Vector2 position = default, Color color = default, float scale = 1)
     {
