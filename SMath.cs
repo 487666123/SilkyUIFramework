0783e7620b77dcf4e471cad32a8fb7f5d0180423
太阳照常升起 <487666123@qq.com>
2026-03-06 22:29:16 +0800

    f**k crlf lf

---
+++ SMath.cs
@@ -0,0 +1,9 @@
+﻿namespace SilkyUIFramework;
+
+public static class SMath
+{
+    public static bool NearlyEqual(this float a, float b, float tolerance = 1e-5f)
+    {
+        return MathF.Abs(a - b) < tolerance;
+    }
+}
