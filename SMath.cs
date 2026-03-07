0cab8301a40d45b980e0ac9aec22ff8e35234136
太阳照常升起 <487666123@qq.com>
2026-03-07 08:18:52 +0800

    SMath

--- SMath.cs
+++ SMath.cs
@@ -4,6 +4,11 @@ public static class SMath
 {
     public static bool NearlyEqual(this float a, float b, float tolerance = 1e-5f)
     {
-        return MathF.Abs(a - b) < tolerance;
+        return Math.Abs(a - b) < tolerance;
+    }
+
+    public static bool NearlyEqual(this double a, double b, double tolerance = 1e-5f)
+    {
+        return Math.Abs(a - b) < tolerance;
     }
 }
