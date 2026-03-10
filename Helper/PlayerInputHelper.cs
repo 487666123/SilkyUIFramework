29f66c39d6bf82841dc36f3e47f7a430c52488c4
太阳照常升起 <487666123@qq.com>
2026-03-10 19:36:46 +0800

    用户输入小于零检测

--- Helper/PlayerInputHelper.cs
+++ Helper/PlayerInputHelper.cs
@@ -2,12 +2,13 @@
 
 public static class PlayerInputHelper
 {
-    public static void SetZoom(Matrix matrix) => SetZoom(in matrix);
-
     public static void SetZoom(in Matrix matrix) => SetZoom(new Vector2(1f / matrix.M11, 1f / matrix.M22));
 
     public static void SetZoom(Vector2 scale)
     {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(scale.X, nameof(scale.X));
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(scale.Y, nameof(scale.Y));
+
         Main.lastMouseX = (int)(PlayerInput._originalLastMouseX * scale.X);
         Main.lastMouseY = (int)(PlayerInput._originalLastMouseY * scale.Y);
         Main.mouseX = (int)(PlayerInput._originalMouseX * scale.X);
--- SMath.cs
+++ SMath.cs
@@ -2,12 +2,12 @@
 
 public static class SMath
 {
-    public static bool NearlyEqual(this float a, float b, float tolerance = 1e-5f)
+    public static bool NearlyEqual(this float a, float b, float tolerance = 1e-6f)
     {
         return Math.Abs(a - b) < tolerance;
     }
 
-    public static bool NearlyEqual(this double a, double b, double tolerance = 1e-5f)
+    public static bool NearlyEqual(this double a, double b, double tolerance = 1e-6)
     {
         return Math.Abs(a - b) < tolerance;
     }
